10c6bccd0d138db891f3dcea95adbc9349d19baa
Piers14 <61315972+piers-hinds@users.noreply.github.com>
2022-01-23 15:03:31 +0000

    min max in dataloaders

--- ubi/data.py
+++ ubi/data.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import numpy as np
 import os
+from sklearn.preprocessing import MinMaxScaler
 
 
 class UbiquantDataset(Dataset):
@@ -23,8 +24,20 @@ class UbiquantDataset(Dataset):
                 torch.tensor(data[self.target].to_numpy(), device=self.device))
 
 
-def get_ubiquant_dataloaders(dir, fnames, train_index, val_index, device='cuda'):
+def get_ubiquant_dataloaders(dir, fnames, train_index, val_index, device='cuda', min_max=None):
     train_dset = UbiquantDataset(dir, fnames.iloc[train_index], device)
     val_dset = UbiquantDataset(dir, fnames.iloc[val_index], device)
-    return (DataLoader(train_dset, batch_size=None, batch_sampler=None),
-            DataLoader(val_dset, batch_size=None, batch_sampler=None))
+    if min_max is not None:
+        return (DataLoader(train_dset, batch_size=None, batch_sampler=None),
+                DataLoader(val_dset, batch_size=None, batch_sampler=None),
+                get_min_max(dir, fnames.iloc[train_index]))
+    else:
+        return (DataLoader(train_dset, batch_size=None, batch_sampler=None),
+                DataLoader(val_dset, batch_size=None, batch_sampler=None))
+
+
+def get_min_max(dir, fnames):
+    df = pd.concat((pd.read_parquet(os.path.join(dir, f)) for f in fnames.iloc[:, 0]))
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    scaler.fit(df[['f_' + str(i) for i in range(300)]])
+    return scaler.data_min_, scaler.data_max_
